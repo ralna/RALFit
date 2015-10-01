12c6fa9e7d290df13d3d76af20d3c2128bdde688
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-10-01 12:12:30 +0100

    Call ral_nlls from driver

--- src/driver.f90
+++ src/driver.f90
@@ -2,7 +2,35 @@ program driver
 
 use nlls_module
 
-write(*,*) 'RAL NLLS driver'
+integer                   :: n, m, len_work_int, len_work_real
+real(wp), allocatable     :: X(:), Work_real(:)
+integer, allocatable      :: Work_int(:)
+type( NLLS_inform_type )  :: status
+type( NLLS_control_type ) :: options
 
+write(*,*) '==============='
+write(*,*) 'RAL NLLS driver'
+write(*,*) '==============='
+write(*,*) ' '
+
+
+n = 100
+
+m = 90
+
+len_work_int = n
+allocate( Work_int(len_work_int) )
+
+len_work_real = n
+allocate( Work_real(len_work_real) ) 
+
+allocate( X(n) )
+
+options%print_level = 3
+
+call ral_nlls(n, m, X, Work_int, len_work_int, & 
+              Work_real, len_work_real,         &
+              eval_F, eval_J,                   &
+              status, options )
 
 end program driver
