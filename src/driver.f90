055f1f673bc196c0169ca2d9a9f69d9923556a1e
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-11-10 12:11:24 +0000

    Minor changes to the driver file

--- src/driver.f90
+++ src/driver.f90
@@ -25,8 +25,8 @@ X(1) = 1.0
 X(2) = 2.0
 
 options%print_level = 3
-options%nlls_method = 2
-options%model = 1
+options%nlls_method = 3
+options%model = 3
 options%maxit = 25
 !options%stop_g_relative = 1e-10
 !options%stop_g_absolute = 1e-10
