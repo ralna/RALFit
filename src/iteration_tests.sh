af513c752488f2d6c3bfd8922bbc55bb7ec56431
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-11-30 16:22:51 +0000

    run iteration tests for calling runcutest

--- src/iteration_tests.sh
+++ src/iteration_tests.sh
@@ -2,19 +2,23 @@
 
 # A script to run a sequence of tests
 echo "model = 1"
-sed -i '5s/.*/1		    ! model/' cutest_control.in
+sed -i '5s/.*/  1                   model used (1=first order, 2=Newton)/' \
+    $CUTEST/src/ral_nlls/RAL_NLLS.SPC
 ./run_tests.sh results_m1.out
 
 echo "model = 2"
-sed -i '5s/.*/2		    ! model/' cutest_control.in
+sed -i '5s/.*/  2                   model used (1=first order, 2=Newton)/' \
+    $CUTEST/src/ral_nlls/RAL_NLLS.SPC
 ./run_tests.sh results_m2.out
 
 echo "model = 7"
-sed -i '5s/.*/7		    ! model/' cutest_control.in
+sed -i '5s/.*/  7                   model used (1=first order, 2=Newton)/' \
+    $CUTEST/src/ral_nlls/RAL_NLLS.SPC
 ./run_tests.sh results_m7.out
 
 echo "model = 8"
-sed -i '5s/.*/8		    ! model/' cutest_control.in
+sed -i '5s/.*/  8                   model used (1=first order, 2=Newton)/' \
+    $CUTEST/src/ral_nlls/RAL_NLLS.SPC
 ./run_tests.sh results_m8.out
 
 ./analyse_iterations.py
--- src/run_tests.sh
+++ src/run_tests.sh
@@ -13,20 +13,28 @@ echo "**        R A L _ N L L S          **"
 echo "**                                 **"
 echo "*************************************"
 
-> data/results.out
-> data/results.out_data
+#> data/results.out
+#> data/results.out_data
+cd ../cutest/sif/
+> RAL_NLLS.sum
 cat sif_names.txt | while read file; do
     echo " "
     echo " ~~~~~~~ $file ~~~~~~~~ "
     echo " "
-    make gen_problem SIFFILE=../cutest/sif/$file
-    make all
-    make cutest
-    ./cutest
+#    make gen_problem SIFFILE=../cutest/sif/$file
+#    make all
+#    make cutest
+#    ./cutest
+    runcutest -p ral_nlls --decode $file
 done
 
 if [ $# -eq 1 ]
  then
-    mv data/results.out data/$1
-    mv data/results.out_data data/$1_data
+    mv RAL_NLLS.sum ../../src/data/$1
+#    mv data/results.out_data data/$1_data
+else
+    mv RAL_NLLS.sum ../../src/data/results.out
 fi
+
+cd ../../src/
+
