e4924762960fbf66b40f6f0b0b7cb1862b6dfbdc
Jonathan Hogg <jhogg41@gmail.com>
2016-01-15 14:38:33 +0000

    Fix notation

--- libral_nlls/example/Fortran/nlls_example.f90
+++ libral_nlls/example/Fortran/nlls_example.f90
@@ -1,6 +1,6 @@
 ! examples/Fortran/nlls_example.f90
 ! 
-! Attempts to fit the model y_i = x_1 e ^(x_2 t_i)
+! Attempts to fit the model y_i = x_1 e^(x_2 t_i)
 ! For parameters x_1 and x_2, and input data (t_i, y_i)
 module fndef_example
    use nlls_module, only : params_base_type
@@ -14,13 +14,13 @@ module fndef_example
    end type
 
 contains
-   ! Calculate f_i(x; t_i, y_i) = x_1 e^(x_2 * t_i) - y_i
-   subroutine eval_F(status, n, m, x, f, params)
+   ! Calculate r_i(x; t_i, y_i) = x_1 e^(x_2 * t_i) - y_i
+   subroutine eval_r(status, n, m, x, r, params)
       integer, intent(out) :: status
       integer, intent(in) :: n
       integer, intent(in) :: m
       real(wp), dimension(*), intent(in) :: x
-      real(wp), dimension(*), intent(out) :: f
+      real(wp), dimension(*), intent(out) :: r
       class(params_base_type), intent(in) :: params
 
       real(wp) :: x1, x2
@@ -29,11 +29,11 @@ contains
       x2 = x(2)
       select type(params)
       type is(params_type)
-         f(1:m) = x1 * exp(x2*params%t(:)) - params%y(:)
+         r(1:m) = x1 * exp(x2*params%t(:)) - params%y(:)
       end select
 
       status = 0 ! Success
-   end subroutine eval_F
+   end subroutine eval_r
    ! Calculate:
    ! J_i1 = e^(x_2 * t_i)
    ! J_i2 = t_i x_1 e^(x_2 * t_i)
@@ -58,16 +58,15 @@ contains
       status = 0 ! Success
    end subroutine eval_J
    ! Calculate
-   ! f_i(x; t_i, y_i) = x_1 e^(x_2 * t_i) - y_i
-   ! HF = sum_i f_i H_i
+   ! HF = sum_i r_i H_i
    ! Where H_i = [ 1                t_i e^(x_2 t_i)    ]
    !             [ t_i e^(x_2 t_i)  t_i^2 e^(x_2 t_i)  ]
-   subroutine eval_HF(status, n, m, x, f, HF, params)
+   subroutine eval_HF(status, n, m, x, r, HF, params)
       integer, intent(out) :: status
       integer, intent(in) :: n
       integer, intent(in) :: m
       real(wp), dimension(*), intent(in) :: x
-      real(wp), dimension(*), intent(in) :: f
+      real(wp), dimension(*), intent(in) :: r
       real(wp), dimension(*), intent(out) :: HF
       class(params_base_type), intent(in) :: params
 
@@ -77,10 +76,10 @@ contains
       x2 = x(2)
       select type(params)
       type is(params_type)
-         HF(1) = sum(f(1:m) * 1)                                     ! H_11
-         HF(2) = sum(f(1:m) * params%t(:) * exp(x2*params%t(:)))     ! H_21
-         HF(3) = HF(2)                                               ! H_12
-         HF(4) = sum(f(1:m) * params%t(:)**2 * exp(x2*params%t(:)))  ! H_22
+         HF(    1) = sum(r(1:m) * 1)                                     ! H_11
+         HF(    2) = sum(r(1:m) * params%t(:) * exp(x2*params%t(:)))     ! H_21
+         HF(1*n+1) = HF(2)                                               ! H_12
+         HF(1*n+2) = sum(r(1:m) * params%t(:)**2 * exp(x2*params%t(:)))  ! H_22
       end select
 
       status = 0 ! Success
@@ -108,7 +107,7 @@ program nlls_example
    ! Call fitting routine
    control%nlls_method = 4 ! FIXME: remove
    x(:) = (/ 2.5, 0.25 /) ! Initial guess
-   call ral_nlls(2, m, x, eval_F, eval_J, eval_HF, params, info, control)
+   call ral_nlls(2, m, x, eval_r, eval_J, eval_HF, params, info, control)
    if(info%status.ne.0) then
       print *, "ral_nlls() returned with error flag ", info%status
       stop
