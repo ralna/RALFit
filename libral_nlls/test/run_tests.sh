efdbf2298889c6e6139f6c8afd88289a2ae94e27
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2016-02-25 10:49:23 +0000

    add output files to test directory to run examples as tests

--- libral_nlls/test/run_tests.sh
+++ libral_nlls/test/run_tests.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-make -C $RAL_NLLS../debug/
+NLLS_DEBUG=$RAL_NLLS../debug/
+NLLS_TEST_SRC=$RAL_NLLS../test/
+NLLS_TEST=$NLLS_DEBUG/test/
+NLLS_EXAMPLE=$NLLS_DEBUG/example/
+
+make -C $NLLS_DEBUG
 RESULT=$?
 [ $RESULT -ne 0 ] && exit 1
 
 echo "cutest example..."
-cd $RAL_NLLS../test/comparison_tests/cutest/sif/
-runcutest --package ral_nlls --architecture pc64.lnx.gfo --decode RAT43.SIF
+cd $NLLS_TEST_SRC/comparison_tests/cutest/sif/
+runcutest --package ral_nlls --architecture pc64.lnx.gfo --decode RAT43.SIF | \
+    diff $NLLS_TEST_SRC/rat43.output -
+RESULT=$?
+[ $RESULT -ne 0 ] && exit 2
+echo "passed"
 
 # run the test suite...
-$RAL_NLLS../debug/test/test
+$NLLS_TEST/test
 RESULT=$?
-[ $RESULT -ne 0 ] && exit 1
+[ $RESULT -ne 0 ] && exit 3
 
 # run the sample programs...
 echo "C example...."
-$RAL_NLLS../debug/example/C/nlls_c_example
+$NLLS_EXAMPLE/C/nlls_c_example | diff $NLLS_TEST_SRC/nlls_c_example.output -
+RESULT=$?
+[ $RESULT -ne 0 ] && exit 4
+echo "passed"
+
+# $RAL_NLLS../debug/example/C/nlls_c_example
 echo "Fortran example..."
-$RAL_NLLS../debug/example/Fortran/nlls_example
+$NLLS_EXAMPLE/Fortran/nlls_example | diff $NLLS_TEST_SRC/nlls_fortran_example.output -
+RESULT=$?
+[ $RESULT -ne 0 ] && exit 5
+echo "passed"
 
 #[ $RESULT -ne 0 ] && exit 1
 exit 0
