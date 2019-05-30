54efacb357b5e8334f3ceb718b1a75f96113e5b8
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2019-05-30 14:43:26 +0100

    Make python tests follow the pattern of the others
    Keep the source tree clean, and copy the tests over
    into the build tree

--- makebuild.sh
+++ makebuild.sh
@@ -17,10 +17,29 @@ RESULT=$?
 RESULT=$?
 [ $RESULT -ne 0 ] && exit 3
 cd $SCRIPTPATH/libRALFit/
+
+######################
+## run python tests ##
+######################
+
+#cd $SCRIPTPATH/libRALFit/
 export LD_LIBRARY_PATH=$SCRIPTPATH/libRALFit/build/src/:$LD_LIBRARY_PATH
-cd test
-./nlls_python_test
+#cd test
+./nlls_python_test &> nlls_python_test.output
 RESULT=$?
-[ $RESULT -ne 0 ] && exit 4
+[ $RESULT -ne 0 ] && exit 5
+diff nlls_python_test.output $SCRIPTPATH/libRALFit/test/solve_python.output
+RESULT=$?
+if [ $RESULT -gt 1 ] 
+then
+  echo "[Python test]: Something is wrong with the diff"
+  exit 5
+elif [ $RESULT -eq 1 ]
+then
+  echo "[Python test]: Something is wrong with the diff"
+  exit 5
+else
+  echo "** Python test passed successfully **"
+fi
 
 exit 0
