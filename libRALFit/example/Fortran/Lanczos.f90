caf122d5ba1d2f39d8e0a10a9e0466ed4e743d5b
Andrew Sajo <andrew.sajo@nag.co.uk>
2019-06-13 17:29:45 +0100

    Update to Lanczos example with box-bounds

--- libRALFit/example/Fortran/Lanczos.f90
+++ libRALFit/example/Fortran/Lanczos.f90
@@ -1,15 +1,22 @@
 ! examples/Fortran/Lanczos.f90
 
 module lanczos_module
-  use ral_nlls_double, only : params_base_type
+  use ral_nlls_double, only : params_base_type, params_box_type
   implicit none
 
   integer, parameter :: wp = kind(0d0)
 
+  ! Unconstrained version
   type, extends(params_base_type) :: params_type
      real(wp), dimension(:), allocatable :: t ! The m data points t_i
      real(wp), dimension(:), allocatable :: y ! The m data points y_i
   end type params_type
+
+  ! Constrained version
+  type, extends(params_box_type) :: params_b_type
+     real(wp), dimension(:), allocatable :: t ! The m data points t_i
+     real(wp), dimension(:), allocatable :: y ! The m data points y_i
+  end type params_b_type
 
 contains
 
@@ -25,14 +32,20 @@ contains
 
     select type(params)
     type is(params_type)
-       r(1:m) = params%y(:) &
-            - x(1)*exp(-x(2)*params%t(:)) &
-            - x(3)*exp(-x(4)*params%t(:)) &
-            - x(5)*exp(-x(n)*params%t(:))
+      r(1:m) = params%y(:) &
+        - x(1)*exp(-x(2)*params%t(:)) &
+        - x(3)*exp(-x(4)*params%t(:)) &
+        - x(5)*exp(-x(n)*params%t(:))
+      status = 0 ! success
+    type is(params_b_type)
+        r(1:m) = params%y(:) &
+          - x(1)*exp(-x(2)*params%t(:)) &
+          - x(3)*exp(-x(4)*params%t(:)) &
+          - x(5)*exp(-x(n)*params%t(:))
+        status = 0 ! success
+    class default
+      status = -1
     end select
-
-    status = 0 ! success
-    
   end subroutine eval_r
 
   subroutine eval_J(status, n, m, x, J, params)
@@ -51,10 +64,18 @@ contains
          J(3*m+1:4*m) = +params%t(1:m) * x(3) * exp(-x(4)*params%t(1:m))! J_i4
          J(4*m+1:5*m) = -exp(-x(6)*params%t(1:m))                     ! J_i5
          J(5*m+1:6*m) = +params%t(1:m) * x(5) * exp(-x(n)*params%t(1:m))! J_i6
+         status = 0 ! Success
+    type is(params_b_type)
+         J(    1:  m) = -exp(-x(2)*params%t(1:m))                     ! J_i1
+         J(  m+1:2*m) = params%t(1:m) * x(1) * exp(-x(2)*params%t(1:m))! J_i2
+         J(2*m+1:3*m) = -exp(-x(4)*params%t(1:m))                     ! J_i3
+         J(3*m+1:4*m) = +params%t(1:m) * x(3) * exp(-x(4)*params%t(1:m))! J_i4
+         J(4*m+1:5*m) = -exp(-x(6)*params%t(1:m))                     ! J_i5
+         J(5*m+1:6*m) = +params%t(1:m) * x(5) * exp(-x(n)*params%t(1:m))! J_i6
+         status = 0 ! Success
+    Class Default
+         status = -1
     end select
-
-
-    status = 0 ! Success
   end subroutine eval_J
 
   subroutine eval_HF(status, n, m, x, r, HF, params)
@@ -78,9 +99,21 @@ contains
          HF(4*n+6) = sum( r(1:m) * params%t(1:m) * exp(-x(6)*params%t(1:m)))  ! H_65
          HF(5*n+5) = HF(4*n + 6)                                                     ! H_56
          HF(5*n+6) = sum(-r(1:m) * (params%t(1:m)**2) * x(5) * exp(-x(6)*params%t(1:m)))! H_66
+         status = 0 ! Success
+      type is(params_b_type)
+         HF(    2) = sum( r(1:m) * params%t(1:m) * exp(-x(2)*params%t(1:m)))  ! H_21
+         HF(1*n+1) = HF(2)                                                     ! H_12
+         HF(1*n+2) = sum(-r(1:m) * (params%t(1:m)**2) * x(1) * exp(-x(2)*params%t(1:m)))! H_22
+         HF(2*n+4) = sum( r(1:m) * params%t(1:m) * exp(-x(4)*params%t(1:m)))  ! H_43
+         HF(3*n+3) = HF(2*n+4)                                                 ! H_34
+         HF(3*n+4) = sum(-r(1:m) * (params%t(1:m)**2) * x(3) * exp(-x(4)*params%t(1:m)))! H_44
+         HF(4*n+6) = sum( r(1:m) * params%t(1:m) * exp(-x(6)*params%t(1:m)))  ! H_65
+         HF(5*n+5) = HF(4*n + 6)                                                     ! H_56
+         HF(5*n+6) = sum(-r(1:m) * (params%t(1:m)**2) * x(5) * exp(-x(6)*params%t(1:m)))! H_66
+         status = 0 ! Success
+    Class Default
+         status = -1
       end select
-
-      status = 0 ! Success
     end subroutine eval_HF
 
     subroutine eval_HP(status, n, m, x, y, HP, params)
@@ -108,12 +141,24 @@ contains
             HP((n-1)*i + 6) = params%t(i) * exp(-x(6)*params%t(i)) * y(5) - &
                  (params%t(i)**2) * x(5) * exp(-x(6)*params%t(i)) * y(6)
          end do
+         status = 0 ! Success
+      type is (params_b_type)
+         do i = 1, m
+            HP((n-1)*i + 1) = params%t(i) * exp(-x(2)*params%t(i)) * y(2)
+            HP((n-1)*i + 2) = params%t(i) * exp(-x(2)*params%t(i)) * y(1) - &
+                 (params%t(i)**2) * x(1) * exp(-x(2)*params%t(i)) * y(2)
+            HP((n-1)*i + 3) = params%t(i) * exp(-x(4)*params%t(i)) * y(4)
+            HP((n-1)*i + 4) = params%t(i) * exp(-x(4)*params%t(i)) * y(3) - &
+                 (params%t(i)**2) * x(3) * exp(-x(4)*params%t(i)) * y(4)
+            HP((n-1)*i + 5) = params%t(i) * exp(-x(6)*params%t(i)) * y(6)
+            HP((n-1)*i + 6) = params%t(i) * exp(-x(6)*params%t(i)) * y(5) - &
+                 (params%t(i)**2) * x(5) * exp(-x(6)*params%t(i)) * y(6)
+         end do
+         status = 0 ! Success
+    Class Default
+         status = -1
       end select
-         
-      
     end subroutine eval_HP
-    
-  
 end module lanczos_module
 
 
@@ -127,9 +172,10 @@ program lanczos
   type(nlls_options) :: options
   type(nlls_inform) :: inform
 
-  integer :: m,n
-  real(wp), allocatable :: x(:)
+  integer :: m,n,i
+  real(wp), allocatable :: x(:), blx(:), bux(:)
   type(params_type) :: params
+  type(params_b_type) :: params_box
   real(wp) :: tic, toc
   
   ! data to be fitted
@@ -190,31 +236,86 @@ program lanczos
   allocate(x(n))
   x = (/ 1.2, 0.3, 5.6, 5.5, 6.5, 7.6 /) ! SP 1
 
-  options%print_level = 1
+  options%print_level = 3
   options%exact_second_derivatives = .true.
-  options%model = 4
-  options%nlls_method = 3
+  options%model = 3
+  options%nlls_method = 4!3
   options%use_ews_subproblem = .true.
   options%type_of_method = 2
   options%regularization_term = 1.0e-2
   options%regularization_power = 2.0
-  options%reg_order = -1.0
+  options%reg_order = 2.0
   options%inner_method = 2
   options%maxit = 1000
 
+  print *, "Unconstrained problem"
   call cpu_time(tic)
   call nlls_solve(n,m,x,eval_r, eval_J, eval_HF, params, options, inform)!, eval_HP=eval_HP)
-  if(inform%status.ne.0) then
+  call cpu_time(toc)
+  if (inform%status.ne.0) then
      print *, "ral_nlls() returned with error flag ", inform%status
      stop
   endif
-  call cpu_time(toc)
   
   ! Print result
+  print *, "Unconstrained problem (solution)"
   print *, "Found a local optimum at x = ", x
   print *, "Took ", inform%iter, " iterations"
   print *, "     ", inform%f_eval, " function evaluations"
   print *, "     ", inform%g_eval, " gradient evaluations"
   print *, "     ", inform%h_eval, " hessian evaluations"
   print *, "     ", toc-tic, " seconds"
+
+  print *
+! stop
+  ! Extend the problem to include bounds on the variables
+  print *, "Constrained problem"
+  x = (/ 1.2, 0.3, 5.6, 5.5, 6.5, 7.6 /) ! SP 1
+ 
+  ! Add some box bound
+  Allocate(blx(n),bux(n),params_box%t(m),params_box%y(m))
+
+  ! Copy problem data
+  params_box%y(1:m) = params%y(1:m)
+  params_box%t(1:m) = params%t(1:m)
+
+  blx(1:n) = -1.0e20_wp
+  bux(1:n) = +1.0e20_wp
+  bux(1) = 1.445_wp
+  blx(5) = -3.9e-1_wp
+  bux(5) = -0.200_wp
+  bux(6) = 10.0_wp
+  bux(6) = 4.640_wp
+
+  Call nlls_setup_bounds(params_box, n, blx, bux, options, inform)
+  if (inform%status/=0) then
+    Write(*,*) 'ERROR: nlls_setup_bounds failed, status=', inform%status
+    stop
+  End if
+
+  call cpu_time(tic)
+  call nlls_solve(n,m,x,eval_r, eval_J, eval_HF, params_box, options, inform)
+  call cpu_time(toc)
+  if(inform%status.ne.0) then
+     print *, "ral_nlls() returned with error flag ", inform%status
+     stop
+  endif
+  ! Print result
+  print *, "Constrained problem (solution)"
+  print *, "Objective Value at solution    = ", inform%obj
+  print *, "Objective Gradient at solution = ", inform%norm_g
+  print *, "Found a local optimum at x:"
+  Write (*, Fmt=99998) "Idx", "blx(i)", "x(i)", "bux(i)"
+  Do i = 1, n
+    Write(*,Fmt=99999) i, blx(i), x(i), bux(i)
+  End Do
+  print *, "Took ", inform%iter, " iterations (LS:",inform%ls_step_iter,' PG:',inform%pg_step_iter,')'
+  print *, "     ", inform%f_eval, " function evaluations (LS:",inform%f_eval_ls,' PG:',inform%f_eval_pg,')'
+  print *, "     ", inform%g_eval, " gradient evaluations (LS:",inform%g_eval_ls,' PG:',inform%g_eval_pg,')'
+  print *, "     ", inform%h_eval, " hessian evaluations"
+  print *, "     ", toc-tic, " seconds"
+
+99999  Format (5X,I3,2X,3(Es13.6e2,2X))
+99998  Format (5X,A3,2X,3(A13,2X))
+
 end program lanczos
