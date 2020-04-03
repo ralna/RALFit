8886dbac684298c8ed3020e70b47687a7229579d
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2020-04-03 14:01:33 +0100

    change number of sig figs reported in example/test

--- libRALFit/example/Python/solve_box.py
+++ libRALFit/example/Python/solve_box.py
@@ -56,7 +56,7 @@ x0 = numpy.array([2.5, 0.25])
 # Print result
 print("Found a local optimum at x = [ {0:.8f}  {1:.8f} ]".format(x[0],x[1]))
 print("RALFit converged in {:d} iterations".format(inform["iter"]))
-print("The cost function is {:f} at the minimum, and ||J^Tr||={:e}"
+print("The cost function is {:.4f} at the minimum, and ||J^Tr||={:.2e}"
       .format(inform["obj"],inform["norm_g"])
 )
 
