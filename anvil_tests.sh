e24b419812f5d9fcc74106db08442bdce8262f3c
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2025-06-18 14:15:30 +0100

    Fix ci (#126)
    * remove version numbers
    
    * update module versions
    
    * fix issues

--- anvil_tests.sh
+++ anvil_tests.sh
@@ -1,13 +1,15 @@
+#module spider
+#
 echo "Compiler $compiler on target $target"
 case $compiler in
 gfortran)
-   module load gcc/7.2.0
+   module load gcc/latest
    export CC=gcc
    export F77=gfortran
    export FC=gfortran
    ;;
 gfortran-debug)
-   module load gcc/7.2.0
+   module load gcc/latest
    export CC=gcc
    export F77=gfortran
    export FC=gfortran
@@ -16,21 +18,21 @@ gfortran-debug)
    export FFLAGS="-g -O2 -Wall -pedantic -fcheck=all -fbacktrace -fno-omit-frame-pointer -finit-real=nan -finit-integer=-9999 -fopenmp"
    ;;
 ifort)
-   module load intel/2017.4
-   export CC=icc
+   module load intel/latest
+   export CC=icx
    export F77=ifort
    export FC=ifort
    ;;
 nagfor) 
-   module load gcc/7.2.0
-   module load nag/6.2
+   module load gcc/latest
+   module load nag/7.2
    export CC=gcc
    export F77=nagfor
    export FC=nagfor
    ;;
 nagfor-debug) 
-   module load gcc/7.2.0
-   module load nag/6.2
+   module load gcc/latest
+   module load nag/7.2
    export CC=gcc
    export F77=nagfor
    export FC=nagfor
@@ -39,7 +41,7 @@ esac
 
 
 #module load cmake/3.3.1 openblas/0.2.14
-module load cmake/3.7.2 blas/3.5.0 lapack/3.5.0
+module load cmake/latest openblas/latest
 export BLAS_LIBRARIES=-lopenblas
 
 case $compiler in
