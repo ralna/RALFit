f26359c5fb5400c7585b79ee8c3e3c02ed86b4c8
Andrew Sajo <andrew.sajo@amd.com>
2025-10-03 16:22:32 +0100

    Potential fix for AOCC UT?

--- libRALFit/test/example_module.f90
+++ libRALFit/test/example_module.f90
@@ -2155,8 +2155,8 @@ contains
          fails = fails + 1
       end if
 
-      n = 65
-      m = 60
+      n = 115
+      m = 100
       deallocate (J, f, Jd, d)
       allocate (J(n*m), f(m), d(n)) ! f has wrong size
 
