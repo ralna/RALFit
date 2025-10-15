c783df07c6b86323c10890904dc41c9841910da1
Andrew Sajo <andrew.sajo@amd.com>
2025-10-15 14:26:02 +0100

    add parameter to anvil

--- anvil_tests.sh
+++ anvil_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Anvil launch script
-# Usage: anvil_tests.sh <compiler>
+# Usage: anvil_tests.sh <compiler> [<extra_flags>]
 
-echo 'Building using $compiler' $compiler
+echo 'Building using $compiler=' $compiler
+echo 'Extra flags: $CMAKE_EXTRA=' $CMAKE_EXTRA
 
 case $compiler in
 gfortran|gcc)
@@ -10,21 +11,21 @@ gfortran|gcc)
    export CC=gcc
    export F77=gfortran
    export FC=gfortran
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release $CMAKE_EXTRA"
    ;;
 gfortran-debug|gcc-debug)
    module load gcc/latest
    export CC=gcc
    export F77=gfortran
    export FC=gfortran
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug $CMAKE_EXTRA"
    ;;
 ifort|ifx|icx|intel)
    module load intel/latest
    export CC=icx
    export F77=ifx
    export FC=ifx
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release $CMAKE_EXTRA"
    ;;
 nagfor*) 
    module load gcc/latest
@@ -32,21 +33,21 @@ nagfor*)
    export CC=gcc
    export F77=nagfor
    export FC=nagfor
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug $CMAKE_EXTRA"
    ;;
 aocc|amd) 
    module load amd
    export CC=clang
    export F77=flang
    export FC=flang
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Release $CMAKE_EXTRA"
    ;;
 aocc-debug|amd-debug) 
    module load amd
    export CC=clang
    export F77=flang
    export FC=flang
-   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug"
+   export RALFIT_FLAGS="-DCMAKE_BUILD_TYPE=Debug $CMAKE_EXTRA"
    ;;
 *)
     echo "$0 Error: Unknown compiler \"$compiler\"?"
